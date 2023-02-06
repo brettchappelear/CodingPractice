bcb354bc1ef1e003257753562ad0099675053a1e
brettchappelear <chappelear.brett@gmail.com>
2023-02-06 13:30:29 -0500

    testing

---
+++ practice.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class practice {
+    
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println(Arrays.toString(numbers));
+    }
+}
